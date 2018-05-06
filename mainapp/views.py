5aab239fd0c993a4e20b45db41eabe6c5bb84820
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2018-05-06 14:53:25 -0300

    remove .env

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 from mainapp.models import ModelExample
 
 from htmlmin.decorators import minified_response
-# Create your views here.
 
 # @requires_csrf_token
 
 
+@cache_page(30)
 @minified_response
 def index(request):
     examples = ModelExample.objects.filter(active=True)
